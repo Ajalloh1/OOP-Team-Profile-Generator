8d6ae9eb56d6f391898e329e5a20b5862d2beb3d
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-04 17:44:36 -0400

    added variable links

---
+++ index.js
@@ -0,0 +1,12 @@
+///requiring links to files to generate page//
+const Manager = require('./lib/Manager');
+// console.log(Manager);
+const Engineer = require('./lib/Engineer');
+console.log(Engineer);
+const Intern = require('./lib/Intern');
+// console.log(Intern);
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+////variable Team Array for push//
+const teamArray = []; 
