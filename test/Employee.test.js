7121a85800bf357c73e6ac6adca8881faa91300d
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-05 00:33:16 -0400

    added test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -4,3 +4,33 @@ text("Create new employee object", () => {
     const employeeInstance = new Employee();
     expet(typeof (employeeInstance)).toBe("object");
 });
+
+test("gets employee name", () => {
+    const name = "Ahmadu";
+    const employeeInstance = new Employee(name);
+    expect(employeeInstance.name).toBe(name);
+});
+
+test("gets employee Id", () => {
+    const id = 10;
+    const employeeInstance = new Employee("Ahmadu", id);
+    expect(employeeInstance.id).toBe(id);
+});
+
+test("gets employee email", () => {
+    const email = "ahmadughj@jalloh.com";
+    const employeeInstance = new Employee("Ahmadu", 10, email);
+    expect(employeeInstance.email).toBe(email);
+});
+
+test("gets name via getName method", () => {
+    const testValue = "Ahmadu"
+    const employeeInstance = new Employee(testValue);
+    expect(employeeInstance.getName).toBe(testName);
+});
+
+test("gets id via getId method", () => {
+    const testId = 10;
+    const employeeInstance = new Employee("Ahmadu", testId);
+    expect(employeeInstance.getId).toBe(testId);
+})
