ac429aa05213685f17b5e9ab84403fff2be81078
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-05 06:27:27 -0400

    added manager test data

---
+++ test/Manager.test.js
@@ -0,0 +1,24 @@
+///Linking manager to test, requiring mnager file///
+const Engineer = require('../lib/Manager');
+
+test("set office number", () => {
+    const testValue = "20";
+    const employeeInstance = new Manager("Ahmadu Jalloh", 10, "ahmadughj@jalloh.com", testValue, "Manager");
+    expect(employeeInstance.officeNumber).toBe(testValue);
+});
+
+test("setRole object", () => {
+    const testValue = "Manager";
+    const employeeInstance = new Manager("Ahmadu Jalloh", 10, "ahmadughj@jalloh.com", 20, "Manager");
+    expect(employeeInstance.getRole).toBe(testValue);
+});
+
+test("get officeNumber", () => {
+    const testValue = "20";
+    const employeeInstance = new Manager("Ahmadu Jalloh", 10, "ahmadughj@jalloh.com", testValue, "Manager");
+    expect(employeeInstance.getOficeNumber).toBe(testValue);
+});
+
+
+
+
