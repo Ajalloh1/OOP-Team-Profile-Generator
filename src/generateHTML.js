6f3bac2f60288223e6facc4c440dd12d1c463fc3
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-05 09:16:05 -0400

    added engineer card boottrap

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,3 +17,63 @@ function generateCard(teamArray) {
             </div>
     `
     };
+
+    // create engineer card//
+    const generateEngineer = function (engineer) {
+        return `
+            <div class="card m-3" style="width: 16rem;">
+                <div class="card-body bg-primary text-light">
+                    <h4 class="card-title">${engineer.getName()}</h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="#"> ${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">${engineer.getGithub()}</li>
+                </ul>
+            </div>
+    `
+    };
+
+    // create intern card//
+
+
+
+
+
+
+    return cardArray.join("")
+}
+module.exports = teamArray => {
+    return `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>My Team</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+            <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        </head>
+        
+        <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 jumbotron mb-3 team-heading">
+                        <h1 class="text-center">Team</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="team-area col-12 d-flex justify-content-center">
+                        ${generateCard(teamArray)}
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>`
+
+}
